14feb420fe442642dd96bb0a32f9eb602eae3991
VISHAL MAKVANA <vishalmakvana7937@gmail.com>
2024-11-06 05:45:21 +0530

    Update AdminLogin.js

--- src/components/auth/Admin/AdminLogin.js
+++ src/components/auth/Admin/AdminLogin.js
@@ -46,7 +46,7 @@ const AdminLogin = () => {
             setErrors(newErrors);
         } else {
             try {
-                const response = await fetch('http://localhost:5000/api/admins/login', {
+                const response = await fetch('https://patient-management-server-1.onrender.com/api/admins/login', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
