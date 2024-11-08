f28bb17b732cf51b5105ba8ed18554e0ac988135
VISHAL MAKVANA <vishalmakvana7937@gmail.com>
2024-11-08 23:18:08 +0530

    Update PrescriptionToolsManage.js

--- src/components/DoctorPanel/PrescriptionToolsManage.js
+++ src/components/DoctorPanel/PrescriptionToolsManage.js
@@ -70,10 +70,11 @@ const PrescriptionToolsManage = () => {
                             {["todayPrescription", "olderPrescription"].map((tab) => (
                                 <button
                                     key={tab}
-                                    className={`px-6 py-2 text-lg font-semibold transition duration-300 rounded-md ${activeTab === tab
-                                        ? " text-gray-400"
-                                        : "bg-white  hover:bg-blue-100"
-                                        }`}
+                                    className={`px-6 py-2 text-lg font-semibold transition duration-300 ${activeTab === tab
+                                        ? "text-[#0EABEB] border-b-4 border-[#0EABEB]"
+                                        : "text-gray-600 border-b-4 border-transparent"
+                                    }`}
+                                    
                                     onClick={() => setActiveTab(tab)}
                                 >
                                     {tab === "todayPrescription" ? "Today Prescription" : "Older Prescription"}
